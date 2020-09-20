6c9c8ee031dd9727c6e9e8ac0865b08bc63973e3
valeriia-lun <61375793+valeriia-lun@users.noreply.github.com>
2020-09-20 15:56:43 +0300

    json dependency

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,12 @@
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
+import java.io.FileWriter;
+import java.io.IOException;
 
-
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 public class Main {
 
     public static void main(String[] args) throws IOException {
@@ -25,6 +29,41 @@ public class Main {
                 break;
             }
             // System.out.println(name);
+
+
+
+            //First Employee
+            JSONObject employeeDetails = new JSONObject();
+            employeeDetails.put("firstName", "Lokesh");
+            employeeDetails.put("lastName", "Gupta");
+            employeeDetails.put("website", "howtodoinjava.com");
+
+            JSONObject employeeObject = new JSONObject();
+            employeeObject.put("employee", employeeDetails);
+
+            //Second Employee
+            JSONObject employeeDetails2 = new JSONObject();
+            employeeDetails2.put("firstName", "Brian");
+            employeeDetails2.put("lastName", "Schultz");
+            employeeDetails2.put("website", "example.com");
+
+            JSONObject employeeObject2 = new JSONObject();
+            employeeObject2.put("employee", employeeDetails2);
+
+            //Add employees to list
+            JSONArray employeeList = new JSONArray();
+            employeeList.add(employeeObject);
+            employeeList.add(employeeObject2);
+
+            //Write JSON file
+//            try (FileWriter file = new FileWriter("employees.json")) {
+//
+//                file.write(employeeList.toJSONString());
+//                file.flush();
+//
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
         }
 
 
