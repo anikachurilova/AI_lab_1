ad489be7a5270e292c7d25fbe7b9f1c69a4bec08
Admin <kalinkaaaa14@gmail.com>
2020-09-22 23:12:22 +0300

    make random questions + fixed bugs

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,7 +10,15 @@ import java.io.FileNotFoundException;
 
 public class Main {
 
-
+    static String[] randomQuestions = new String[]{
+            "Why are you asking that?",
+            "Why do you think so?",
+            "Are you sure?",
+            "I don't think this is a good idea",
+            "I'd like to know this info too!",
+            "Do you really want to know that?",
+            "Do you think I should know the answer?",
+            "Sorry, haven't enough time to talk with you."};
 
     public static void main(String[] args) throws Exception
     {
@@ -30,57 +38,46 @@ public class Main {
                     JSONArray employeeList1 = (JSONArray) obj1;
 
                     boolean patternFound= false;
-                    boolean allPatternsMatched = false;
-//                    if(endOfDialog){
-//                        break;
-//                    }
 
+                    //модальні і загальні питання
                     for (Object emp : employeeList1) {
                         if (str.matches(parsePatternObject((JSONObject) emp).getKey())) {
                             Pair<String, String[]> result = parsePatternObject((JSONObject) emp);
                             int l = result.getRight().length;
-                            //    String sentence="";
-//                           for(int i=2;i<words.length;i++){
-//                               sentence+=words[i]+" ";
-//                           }
+
                             if(result.getRight()[((int) (Math.random() * l))].equals("Bye:(")){
                                 endOfDialog = true;
                             }
-                            System.out.println(result.getRight()[((int) (Math.random() * l))] + replacedString(str.toLowerCase()));
+                            String sentence="";
+                           for(int i=2;i<words.length;i++){
+                               sentence+=words[i]+" ";
+                           }
+                            System.out.println(result.getRight()[((int) (Math.random() * l))] + words[1]+replacedString(words[0])+ sentence);
                             patternFound=true;
-                            allPatternsMatched = true;
                             break;
                         }
                     }
+                    //якщо є шаблон звичайний
                     if(!patternFound) {
                         for (Object emp : employeeList) {
-                            //якщо є щось простеньке по шаблону
                             if (str.matches(parsePatternObject((JSONObject) emp).getKey())) {
                                 Pair<String, String[]> result = parsePatternObject((JSONObject) emp);
                                 int l = result.getRight().length;
                                 System.out.println(result.getRight()[((int) (Math.random() * l))]);
                                 patternFound = true;
-                                allPatternsMatched = true;
                                 if(result.getRight()[((int) (Math.random() * l))].equals("Bye:(")){
                                     endOfDialog = true;
                                 }
                                 break;
                             }
                         }
-                        if(!patternFound){
-                            //питання типу where why whose what
-//                            String sentence="";
-//                            for(int i=3;i<words.length;i++){
-//                                sentence+=words[i]+" ";
-//                            }
+                    }
 
-                            System.out.println("Do you wanna know "+ replacedString(str.toLowerCase()));
-                            allPatternsMatched = true;
-                        }
+                    if(!patternFound){
+
+                        System.out.println(randomQuestions[(int)(Math.random()*8)]);
                     }
-                    if (!allPatternsMatched){
-                        System.out.println("Give me more information, please, I can't fully understand you :(");
-                    }
+
                 } catch (FileNotFoundException e) {
                     e.printStackTrace();
                 } catch (IOException e) {
@@ -95,7 +92,7 @@ public class Main {
     }
 
     private static String replacedString(String str){
-        return str.replace("i","you").replace("you","i"). replace("myself","yourself").replace("yourself", "myself")
+        return str.replace("?","").replace("i","you").replace("you","i"). replace("myself","yourself").replace("yourself", "myself")
                 .replace("mine","yours").replace("yours","mine").replace("my","your").replace("your","my").replace("me","you");
     }
 
@@ -112,55 +109,6 @@ public class Main {
         Pair<String,String[]> p = new MutablePair<>(pattern,array);
         return p;
     }
-
-/*    private static String changePronouns(String word){
-        switch (word){
-            case "i":
-                word="you";
-                break;
-            case "you":
-                word="i";
-                break;
-            case "myself":
-                word="yourself";
-                break;
-            case "yourself":
-                word="myself";
-                break;
-            case "mine":
-                word="yours";
-                break;
-            case "yours":
-                word="mine";
-                break;
-            case "my":
-                word="your";
-                break;
-            case "your":
-                word="my";
-                break;
-            case "me":
-                word="you";
-                break;
-            default:
-                break;
-        }
-        return word;
-    }
-    */
-
-//    private static Pair<String,String> parseUnknownPatternObject(JSONObject obj)
-//    {
-//        System.out.println("Obj is "+obj);
-//        JSONObject patternObject = (JSONObject) obj.get("unknownPhrases");
-//        System.out.println(patternObject);
-//        String pattern = (String) patternObject.get("pattern");
-//
-//        String answer = (String) patternObject.get("answer");
-//
-//        Pair<String,String> p = new MutablePair<>(pattern,answer);
-//        return p;
-//    }
 
 
 }
